3a753cd24d48a298d0ac6f7c6ba98fc0a36b36a0
konnecthassan <hassanurrehmanlive@gmail.com>
2024-10-29 01:15:46 +0500

    second commit

---
+++ CalculatorMain.java
@@ -0,0 +1,9 @@
+public class CalculatorMain {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        int sum = calculator.add(10, 20);
+        int product = calculator.multiply(10, 20);
+        System.out.println("Sum: " + sum);
+        System.out.println("Product: " + product);
+    }
+}
